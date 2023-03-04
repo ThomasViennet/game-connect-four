d1c31b6c06d79b6776fa5b485e390e3597d7f799
ThomasViennet <viennet.t@gmail.com>
2023-03-04 21:45:26 +0100

    test

--- templates/front/homepage.php
+++ templates/front/homepage.php
@@ -2,7 +2,8 @@
 
 <?php ob_start(); ?>
 <h1>Welcome to Connect Four</h1>
-<a href="?action=game&id=1">play </a>
+<a href="?action=game&id=1">play</a>
+<p>Test</p>
 
 <?php $content = ob_get_clean(); ?>
 
